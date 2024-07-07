7963b2f2161f15e0a44145fbf1c32867db8562f1
Shyshkanova2005 <ivanovasasha883@gmail.com>
2024-07-07 16:52:07 +0300

    final calculator

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -24,6 +24,7 @@ function App() {
        <Body/>
        <Data/> */}
        <SimpleCalculator/>
+       
     </div>
 
    
--- vite-project/src/Calculator.jsx
+++ vite-project/src/Calculator.jsx
@@ -5,19 +5,32 @@ export default function  SimpleCalculator()  {
   const [num2, setNum2] = useState('');
   const [result, setResult] = useState('');
 
-  const handleCalculate = () => {
+  const Sum = () => {
     const res = parseFloat(num1) + parseFloat(num2);
     setResult(res.toString());
   };
 
-//   const Multiple = () => {
-//     const res = parseFloat(num1) * parseFloat(num2);
-//     setResult(res.toString());
-//   };
+  const Substruct = () => {
+    const res = parseFloat(num1) - parseFloat(num2);
+    setResult(res.toString());
+  };
+
+
+ const Multiple = () => {
+     const res = parseFloat(num1) * parseFloat(num2);
+   setResult(res.toString());
+  };
+
+  const Division = () => {
+    const res = parseFloat(num1) / parseFloat(num2);
+  setResult(res.toString());
+ };
+
+
 
   return (
     <div>
-      <h2>Простий калькулятор</h2>
+      <h2>Suma</h2>
       <input
         type="number"
         placeholder="Введіть перше число"
@@ -30,11 +43,27 @@ export default function  SimpleCalculator()  {
         value={num2}
         onChange={(e) => setNum2(e.target.value)}
       />
-      <button onClick={handleCalculate}>Обчислити</button>
+      <button onClick={Sum}>Обчислити</button>
       <p>Результат: {result}</p>
 
 
-      {/* <p>Multiple</p>
+      <h2>Substruct</h2>
+      <input
+        type="number"
+        placeholder="Введіть перше число"
+        value={num1}
+        onChange={(e) => setNum1(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Введіть друге число"
+        value={num2}
+        onChange={(e) => setNum2(e.target.value)}
+      />
+      <button onClick={Substruct}>Обчислити</button>
+      <p>Результат: {result}</p> 
+
+       <h2>Multiple</h2>
       <input
         type="number"
         placeholder="Введіть перше число"
@@ -48,7 +77,23 @@ export default function  SimpleCalculator()  {
         onChange={(e) => setNum2(e.target.value)}
       />
       <button onClick={Multiple}>Обчислити</button>
-      <p>Результат: {result}</p> */}
+      <p>Результат: {result}</p> 
+
+      <h2>Division</h2>
+      <input
+        type="number"
+        placeholder="Введіть перше число"
+        value={num1}
+        onChange={(e) => setNum1(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Введіть друге число"
+        value={num2}
+        onChange={(e) => setNum2(e.target.value)}
+      />
+      <button onClick={Division}>Обчислити</button>
+      <p>Результат: {result}</p> 
     </div>
   );
 };
